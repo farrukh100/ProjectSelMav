a9031f4fd89db97764c96dbc815fe62bd53af5b4
farrukh100 <farrukhcontour@gmail.com>
2019-11-19 16:32:43 +0500

    driver links dynamic now

--- ProjectSelMav/src/test/java/Automation/NewTest.java
+++ ProjectSelMav/src/test/java/Automation/NewTest.java
@@ -17,13 +17,15 @@ public class NewTest {
   public void f() {
 	  
 	  
-	  String service = "..\\ProjectSelMav\\drivers\\chromedriver\\chromedriver.exe";
+	  //String service = "..\\ProjectSelMav\\drivers\\chromedriver\\chromedriver.exe";
+	  String service = ".\\drivers\\chromedriver\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", service);
 		driver = new ChromeDriver();
 		
       logger=Logger.getLogger("DPCRM");
 		
-		PropertyConfigurator.configure("..\\ProjectSelMav\\Log4j.properties.txt");
+		//PropertyConfigurator.configure("..\\ProjectSelMav\\Log4j.properties.txt");
+		PropertyConfigurator.configure(".\\Log4j.properties.txt");
 		logger.info("Browser Opened");
 	  
 		driver.get("http://www.wikipedia.org");
